38e328b3eb2a2594189d35c45b664f3e9fa27d8d
tazo90 <tazo90@gmail.com>
2022-02-21 22:07:09 +0100

    Added constants

---
+++ src/constants/countries.ts
@@ -0,0 +1,88 @@
+import { icons } from '.';
+
+const { flags } = icons;
+
+const countries = [
+  {
+    value: 'pl',
+    label: 'Poland',
+    img: flags.pl,
+  },
+  {
+    value: 'cz',
+    label: 'Czech Republic',
+    img: flags.cz,
+  },
+  {
+    value: 'hu',
+    label: 'Hungary',
+    img: flags.hu,
+  },
+  {
+    value: 'sk',
+    label: 'Slovak Republic',
+    img: flags.sk,
+  },
+  {
+    value: 'bg',
+    label: 'Bulgaria',
+    img: flags.bg,
+  },
+  {
+    value: 'de',
+    label: 'Germany',
+    img: flags.de,
+  },
+  {
+    value: 'es',
+    label: 'Spain',
+    img: flags.es,
+  },
+  {
+    value: 'ro',
+    label: 'Romania',
+    img: flags.ro,
+  },
+  {
+    value: 'ru',
+    label: 'Russia',
+    img: flags.ru,
+  },
+  {
+    value: 'rs',
+    label: 'Serbia',
+    img: flags.rs,
+  },
+  {
+    value: 'cn',
+    label: 'China',
+    img: flags.cn,
+  },
+  {
+    value: 'fr',
+    label: 'France',
+    img: flags.fr,
+  },
+  {
+    value: 'hr',
+    label: 'Croatia',
+    img: flags.hr,
+  },
+  {
+    value: 'at',
+    label: 'Austria',
+    img: flags.at,
+  },
+  {
+    value: 'pt',
+    label: 'Portugal',
+    img: flags.pt,
+  },
+  {
+    value: 'si',
+    label: 'Slovenia',
+    img: flags.si,
+  },
+];
+
+export default countries;
