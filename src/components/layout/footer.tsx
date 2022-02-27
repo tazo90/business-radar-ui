652cf4b9e328282b3f015428e773b455291b23c5
tazo90 <tazo90@gmail.com>
2022-02-28 00:59:01 +0100

    Add footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <footer className="footer px-4 py-6">
+      <div className="footer-content">
+        <p className="text-sm text-gray-600 text-center">
+          © Brandname 2020. All rights reserved.{" "}
+          <a href="https://twitter.com/iaminos">by iAmine</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
