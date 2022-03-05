5977dcd0a728763fd770d5e43966108a3082ccc2
tazo90 <tazo90@gmail.com>
2022-03-05 19:50:40 +0100

    Add store list row

--- src/pages/zendmap.tsx
+++ src/pages/zendmap.tsx
@@ -7,11 +7,180 @@ import countries from "../constants/countries";
 import Image from "next/image";
 import restaurantImg from "../assets/restaurant.jpg";
 
+function StoreListRow({ store }) {
+  return (
+    <li>
+      <article
+        tabIndex="0"
+        className="flex cursor-pointer border rounded-md p-2 bg-gray-600 text-gray-700 mb-2 hover:border-green-600 focus:outline-none focus:border-green-500"
+      >
+        {/* Store image */}
+        <div className="flex relative h-24 w-[25%]">
+          <Image
+            className="rounded-2xl"
+            src={restaurantImg}
+            alt="Picture of the restaurant"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        {/* Store details info */}
+        <div className="flex flex-col h-full mx-2">
+          <div className="flex items-center pl-3 pr-4 h-1/4 w-fit rounded-3xl bg-gray-100">
+            <div className="flex relative h-7 w-7">
+              <Image
+                src={icons.amrest.markers[store.brand]}
+                alt="Picture of the restaurant"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <div className="flex flex-col items-center ml-2 py-0.5">
+              <span className="text-sm font-semibold leading-none">
+                {store.brand.toUpperCase()}
+              </span>
+              <span className="text-sm leading-none">{store.name}</span>
+            </div>
+            <div className="relative bottom-2 left-1 w-2 h-2 rounded-full bg-green-700"></div>
+          </div>
+          <div className="text-gray-100 text-[0.82rem] leading-snug pt-1">
+            <p>
+              <span className="font-bold">Address:</span> {store.address}
+            </p>
+            <p>
+              <span className="font-bold">Phone:</span> {store.phone}
+            </p>
+            <p>
+              <span className="font-bold">Hours:</span> 08:00 - 16:00
+            </p>
+          </div>
+        </div>
+        {/* Store summary and rate */}
+        <div className="flex flex-col justify-between text-gray-100 text-xs">
+          {/* Summary */}
+
+          <div className="flex justify-end">
+            <div className="flex flex-col font-semibold">
+              <p>Empl:</p>
+              <p>Jobs:</p>
+              <p>Train:</p>
+            </div>
+            <div className="flex flex-col items-end ml-2">
+              <p>10</p>
+              <p>2</p>
+              <p>90%</p>
+            </div>
+          </div>
+
+          {/* Rates */}
+          <div className="flex flex-col items-end text-gray-100 text-xs font-semibold">
+            <div className="flex items-center">
+              <p>3.5</p>
+              <ul className="flex justify-center ml-1">
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="star"
+                    className="w-3 text-yellow-500 mr-1"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                    ></path>
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="star"
+                    className="w-3 text-yellow-500 mr-1"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                    ></path>
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="star"
+                    className="w-3 text-yellow-500 mr-1"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                    ></path>
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="far"
+                    data-icon="star"
+                    className="w-3 text-yellow-500 mr-1"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"
+                    ></path>
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="far"
+                    data-icon="star"
+                    className="w-3 text-yellow-500"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"
+                    ></path>
+                  </svg>
+                </li>
+              </ul>
+            </div>
+            <div className="flex justify-between font-semibold text-white text-xs">
+              <span>120 reviews</span>
+            </div>
+          </div>
+        </div>
+      </article>
+    </li>
+  );
+}
+
 export default function Zendesk() {
   const { data, isLoading, error } = useStoresQuery({
     brand: "kfc",
     country: "pl",
   });
+
+  console.log("DATA", data);
 
   return (
     <div className="h-screen w-full flex overflow-hidden antialiased text-gray-800 bg-white">
@@ -237,6 +406,10 @@ export default function Zendesk() {
               />
             </div>
             <ul>
+              {data?.features.map((store) => (
+                <StoreListRow store={store.properties} />
+              ))}
+
               <li>
                 <article
                   tabindex="0"
@@ -292,11 +465,11 @@ export default function Zendesk() {
 
                     <div className="flex justify-end">
                       <div className="flex flex-col font-semibold">
-                        <p>Employees:</p>
+                        <p>Empl:</p>
                         <p>Jobs:</p>
-                        <p>Training:</p>
+                        <p>Train:</p>
                       </div>
-                      <div className="flex flex-col items-end">
+                      <div className="flex flex-col items-end ml-2">
                         <p>10</p>
                         <p>2</p>
                         <p>90%</p>
@@ -489,7 +662,7 @@ export default function Zendesk() {
             </nav>
             {/* Content map */}
 
-            {/* <Map cluster={true} locations={data} /> */}
+            <Map cluster={true} locations={data} />
 
             {/* <!-- content footer, currently hidden --> */}
             <footer
