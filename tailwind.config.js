dfaad2471ef5bdf989e56303d81336329ff30c97
tazo90 <tazo90@gmail.com>
2022-03-10 19:43:07 +0100

    Add tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
   theme: {
     screens: {
       'xs': '475px',
