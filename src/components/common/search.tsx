42ebf832857509aa8eb9ec76b5e725088f2f6f93
tazo90 <tazo90@gmail.com>
2022-03-15 00:35:43 +0100

    Allow to clear search value

--- src/components/common/search.tsx
+++ src/components/common/search.tsx
@@ -1,4 +1,5 @@
-import { SearchIcon } from "@heroicons/react/outline";
+import { useRef } from "react";
+import { SearchIcon, XIcon } from "@heroicons/react/outline";
 
 export function Search({
   onSearch,
@@ -6,18 +7,30 @@ export function Search({
   className,
   bgColor = "bg-white",
 }) {
+  const inputRef = useRef();
+
+  function onClear() {
+    inputRef.current.value = "";
+  }
+
   return (
-    <div className={`flex flex-row items-center ${className}`}>
+    <div className={`relative flex flex-row items-center ${className}`}>
       <button
         className={`flex items-center justify-center h-10 w-10 text-gray-500 rounded-l-lg ${bgColor}`}
       >
         <SearchIcon className="h-4 w-4" aria-hidden="true" />
       </button>
       <input
+        ref={inputRef}
         type="text"
         className={`w-full border border-transparent focus:border-transparent focus:ring-0 text-sm h-10 flex items-center rounded-lg rounded-l-none pl-0 pr-2 ${bgColor}`}
         placeholder={placeholder}
         onChange={onSearch}
+      />
+      <XIcon
+        className="absolute top-2.5 right-2.5 w-5 h-5 text-gray-500"
+        aria-hidden="true"
+        onClick={onClear}
       />
     </div>
   );
--- src/components/stores/store-list/store-list-row.tsx
+++ src/components/stores/store-list/store-list-row.tsx
@@ -6,7 +6,7 @@ import { StoreListRowSummary } from "./store-list-row-summary";
 
 export function StoreListRow({ store, isActive }) {
   return (
-    <article className="flex drop-shadow-lg justify-between h-32 cursor-pointer border border-gray-500 rounded-md py-2 px-3 bg-gray-100 dark:bg-gray-600 text-xs text-gray-900 dark:text-gray-100 mb-2 hover:bg-slate-300">
+    <article className="flex drop-shadow-lg justify-between h-32 cursor-pointer border border-gray-500 rounded-md py-2 px-3 bg-gray-100 dark:bg-gray-600 text-xs text-gray-900 dark:text-gray-100 mb-2 hover:bg-slate-300 focus:bg-slate-300">
       <div className="relative h-24 hidden xs:flex">
         <img
           className="rounded-2xl object-cover"
