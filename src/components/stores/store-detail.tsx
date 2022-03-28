371d91105a9542dcad0f18296ca416c80bbfff01
tazo90 <tazo90@gmail.com>
2022-03-29 00:25:21 +0200

    Better styling in store details

--- src/components/stores/store-detail.tsx
+++ src/components/stores/store-detail.tsx
@@ -1,75 +1,78 @@
-
-import { ChevronLeftIcon, FilterIcon, MailIcon, PhoneIcon, SearchIcon } from '@heroicons/react/solid'
-import { useDispatch, useSelector } from 'react-redux'
-
+import {
+  ChevronLeftIcon,
+  FilterIcon,
+  HeartIcon,
+  LocationMarkerIcon,
+  MailIcon,
+  PhoneIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
+import { useDispatch, useSelector } from "react-redux";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const tabs = [
-  { name: 'Store', href: '#', current: true },
-  { name: 'Jobs', href: '#', current: false },
-  { name: 'Employees', href: '#', current: false },
-  { name: 'Reviews', href: '#', current: false },
-  { name: 'Trainings', href: '#', current: false },
-]
+  { name: "Overview", href: "#", current: true },
+  { name: "Jobs", href: "#", current: false },
+  { name: "Employees", href: "#", current: false },
+  { name: "Reviews", href: "#", current: false },
+  { name: "Trainings", href: "#", current: false },
+  { name: "Photos", href: "#", current: false },
+];
 
 const profile = {
-  name: 'Ricardo Cooper',
+  name: "Ricardo Cooper",
   imageUrl:
-    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
   coverImageUrl:
-    'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+    "https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
   about: `
     <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
     <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
   `,
   fields: {
-    Phone: '(555) 123-4567',
-    Email: 'ricardocooper@example.com',
-    Title: 'Senior Front-End Developer',
-    Team: 'Product Development',
-    Location: 'San Francisco',
-    Sits: 'Oasis, 4th floor',
-    Salary: '$145,000',
-    Birthday: 'June 8, 1990',
+    "Located in": "Magnolia",
+    Address: "ul. Legnicka 10",
+    Phone: "(555) 123-4567",
+    Hours: "Closed . Open 8AM Tue",
+    Email: "ricardocooper@example.com",
   },
-}
+};
 
 const team = [
   {
-    name: 'Leslie Alexander',
-    handle: 'lesliealexander',
-    role: 'Co-Founder / CEO',
+    name: "Leslie Alexander",
+    handle: "lesliealexander",
+    role: "Co-Founder / CEO",
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
-    name: 'Michael Foster',
-    handle: 'michaelfoster',
-    role: 'Co-Founder / CTO',
+    name: "Michael Foster",
+    handle: "michaelfoster",
+    role: "Co-Founder / CTO",
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
-    name: 'Dries Vincent',
-    handle: 'driesvincent',
-    role: 'Manager, Business Relations',
+    name: "Dries Vincent",
+    handle: "driesvincent",
+    role: "Manager, Business Relations",
     imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
-    name: 'Lindsay Walton',
-    handle: 'lindsaywalton',
-    role: 'Front-end Developer',
+    name: "Lindsay Walton",
+    handle: "lindsaywalton",
+    role: "Front-end Developer",
     imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
-]
+];
 
 export function StoreDetail() {
-  const dispatch = useDispatch();
   const store = useSelector(
     (state: any) => state.store.selectedStore?.properties
   );
@@ -78,14 +81,40 @@ export function StoreDetail() {
     return null;
   }
 
-  console.log("STORE", store)
-  
+  console.log("STORE", store);
+
   return (
     <article>
       {/* Profile header */}
       <div>
         <div>
-          <img className="h-32 w-full object-cover lg:h-48" src={profile.coverImageUrl} alt="" />
+          <img
+            className="h-32 w-full object-cover lg:h-48"
+            src={profile.coverImageUrl}
+            alt=""
+          />
+          <div className="absolute top-0 right-5 mt-3 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+            <button
+              type="button"
+              className="inline-flex justify-center p-2.5 border border-gray-300 shadow-sm text-sm font-medium rounded-full text-gray-700 bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+            >
+              <HeartIcon
+                className="-ml-1 mr-2 h-5 w-5 text-pink-500"
+                aria-hidden="true"
+              />
+              <span>Follow</span>
+            </button>
+            <button
+              type="button"
+              className="inline-flex justify-center p-2.5 border border-gray-300 shadow-sm text-sm font-medium rounded-full text-gray-700 bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+            >
+              <LocationMarkerIcon
+                className="-ml-1 mr-2 h-5 w-5 text-blue-500"
+                aria-hidden="true"
+              />
+              <span>Directions</span>
+            </button>
+          </div>
         </div>
         <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
@@ -98,28 +127,38 @@ export function StoreDetail() {
             </div>
             <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
               <div className="sm:hidden 2xl:block mt-6 min-w-0 flex-1">
-                <h1 className="text-2xl font-bold text-gray-900 truncate">{store.brand_full} {store.name}</h1>
+                <h1 className="text-2xl font-bold text-gray-900 truncate">
+                  {store.brand_full} {store.name}
+                </h1>
               </div>
               <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
                 <button
                   type="button"
                   className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
                 >
-                  <MailIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                  <MailIcon
+                    className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
                   <span>Message</span>
                 </button>
                 <button
                   type="button"
                   className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
                 >
-                  <PhoneIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                  <PhoneIcon
+                    className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
                   <span>Call</span>
                 </button>
               </div>
             </div>
           </div>
           <div className="hidden sm:block 2xl:hidden mt-6 min-w-0 flex-1">
-            <h1 className="text-2xl font-bold text-gray-900 truncate">{profile.name}</h1>
+            <h1 className="text-2xl font-bold text-gray-900 truncate">
+              {profile.name}
+            </h1>
           </div>
         </div>
       </div>
@@ -135,11 +174,11 @@ export function StoreDetail() {
                   href={tab.href}
                   className={classNames(
                     tab.current
-                      ? 'border-pink-500 text-gray-900'
-                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                    'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
+                      ? "border-pink-500 text-gray-900"
+                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
+                    "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
                   )}
-                  aria-current={tab.current ? 'page' : undefined}
+                  aria-current={tab.current ? "page" : undefined}
                 >
                   {tab.name}
                 </a>
@@ -155,7 +194,9 @@ export function StoreDetail() {
           {Object.keys(profile.fields).map((field) => (
             <div key={field} className="sm:col-span-1">
               <dt className="text-sm font-medium text-gray-500">{field}</dt>
-              <dd className="mt-1 text-sm text-gray-900">{profile.fields[field]}</dd>
+              <dd className="mt-1 text-sm text-gray-900">
+                {profile.fields[field]}
+              </dd>
             </div>
           ))}
           <div className="sm:col-span-2">
@@ -178,13 +219,21 @@ export function StoreDetail() {
               className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500"
             >
               <div className="flex-shrink-0">
-                <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={person.imageUrl}
+                  alt=""
+                />
               </div>
               <div className="flex-1 min-w-0">
                 <a href="#" className="focus:outline-none">
                   <span className="absolute inset-0" aria-hidden="true" />
-                  <p className="text-sm font-medium text-gray-900">{person.name}</p>
-                  <p className="text-sm text-gray-500 truncate">{person.role}</p>
+                  <p className="text-sm font-medium text-gray-900">
+                    {person.name}
+                  </p>
+                  <p className="text-sm text-gray-500 truncate">
+                    {person.role}
+                  </p>
                 </a>
               </div>
             </div>
@@ -192,5 +241,5 @@ export function StoreDetail() {
         </div>
       </div>
     </article>
-  )
+  );
 }
--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { ArrowLeftIcon } from '@heroicons/react/solid'
+import { ArrowLeftIcon } from "@heroicons/react/solid";
 
 import { setStore } from "../../slices/store.slice";
 
@@ -13,7 +13,7 @@ export default function Drawer({ children, isOpen, setDrawerOpen }) {
 
   return (
     <main
-      className={`max-w-[52rem] absolute overflow-hidden inset-0 transform ease-in-out ${
+      className={`max-w-[56rem] absolute overflow-hidden inset-0 transform ease-in-out ${
         isOpen
           ? "transition-opacity opacity-100 duration-500 -translate-x-0"
           : "transition-all delay-500 opacity-0 -translate-x-full"
@@ -25,9 +25,15 @@ export default function Drawer({ children, isOpen, setDrawerOpen }) {
         }`}
       >
         <article className="relative w-screen max-w-full pb-10 flex flex-col overflow-y-auto h-full">
-          <button className="flex justify-center items-center absolute top-4 left-4 w-8 h-8 rounded-md bg-stone-600 " onClick={() => onClose()}>
-            <ArrowLeftIcon className="h-6 w-6 text-white" aria-hidden="true" />
-          </button>          
+          <button
+            className="flex justify-center items-center absolute top-4 left-4 w-8 h-8 rounded-md bg-white "
+            onClick={() => onClose()}
+          >
+            <ArrowLeftIcon
+              className="h-6 w-6 text-gray-700"
+              aria-hidden="true"
+            />
+          </button>
           {children}
         </article>
       </section>
