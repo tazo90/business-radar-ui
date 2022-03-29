3c8dae6d77faea386efb2afec076d23344a33a5f
tazo90 <tazo90@gmail.com>
2022-03-29 23:40:52 +0200

    Improve store detail page

--- src/components/stores/store-detail/store-detail.tsx
+++ src/components/stores/store-detail/store-detail.tsx
@@ -1,3 +1,4 @@
+import { ListboxItemRating } from "@components/listbox/listbox-item/listbox-item-rating";
 import {
   ChevronLeftIcon,
   FilterIcon,
@@ -25,6 +26,7 @@ const tabs = [
   },
   { name: "Jobs", href: "#", content: <StoreDetailJobs /> },
   { name: "Employees", href: "#", content: <StoreDetailEmployees /> },
+  { name: "Menu", href: "#", content: null },
   { name: "Reviews", href: "#", content: null },
   { name: "Trainings", href: "#", content: null },
   { name: "Photos", href: "#", content: null },
@@ -79,6 +81,104 @@ const team = [
       "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
 ];
+
+function Reviews() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center">
+        <ul className="flex justify-center ml-1">
+          <li>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="star"
+              className="w-3 text-yellow-500 mr-1"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                fill="currentColor"
+                d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+              ></path>
+            </svg>
+          </li>
+          <li>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="star"
+              className="w-3 text-yellow-500 mr-1"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                fill="currentColor"
+                d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+              ></path>
+            </svg>
+          </li>
+          <li>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="star"
+              className="w-3 text-yellow-500 mr-1"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                fill="currentColor"
+                d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+              ></path>
+            </svg>
+          </li>
+          <li>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="star"
+              className="w-3 text-yellow-500 mr-1"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                fill="currentColor"
+                d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"
+              ></path>
+            </svg>
+          </li>
+          <li>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="star"
+              className="w-3 text-yellow-500"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                fill="currentColor"
+                d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"
+              ></path>
+            </svg>
+          </li>
+        </ul>
+        <span className="text-sm font-semibold pl-2">3.5</span>
+      </div>
+      <p className="text-sm font-semibold text-right">120 reviews</p>
+    </div>
+  );
+}
 
 export function StoreDetail() {
   const [currentTab, setCurrentTab] = useState(tabs[0]);
@@ -138,11 +238,17 @@ export function StoreDetail() {
                 alt=""
               />
             </div>
-            <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+            <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6">
               <div className="sm:hidden 2xl:block mt-6 min-w-0 flex-1">
                 <h1 className="text-2xl font-bold text-gray-900 truncate">
                   {store.brand_full} {store.name}
                 </h1>
+                <div className="flex flex-col">
+                  <span className="text-sm font-semibold pr-4">
+                    <span className="text-green-600 font-semibold">Open</span> -
+                    Closes 8 PM
+                  </span>
+                </div>
               </div>
               <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
                 <button
@@ -165,6 +271,7 @@ export function StoreDetail() {
                   />
                   <span>Call</span>
                 </button>
+                <Reviews />
               </div>
             </div>
           </div>
