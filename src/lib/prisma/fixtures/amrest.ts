2c5f9726d893f2bd6c4b3ba9dc1baa5f52dd4c34
tazo90 <tazo90@gmail.com>
2022-04-16 19:54:41 +0200

    Update seed file

--- src/lib/prisma/fixtures/amrest.ts
+++ src/lib/prisma/fixtures/amrest.ts
@@ -14,30 +14,114 @@ const brands = [
     name: "PH",
     fullName: "Pizza Hut",
   },
+  {
+    name: "BK",
+    fullName: "Burger King",
+  },
+  {
+    name: "SBX",
+    fullName: "Starbucks",
+  },
+  {
+    name: "TAG",
+    fullName: "La Tagliatella",
+  },
+  {
+    name: "BF",
+    fullName: "Blue Frog",
+  },
+  {
+    name: "Kaab",
+    fullName: "Kaab",
+  },
+  {
+    name: "BCA",
+    fullName: "Bacoa",
+  },
+  {
+    name: "SSG",
+    fullName: "Sushi Shop",
+  },
 ];
 
 const countries = [
   {
+    code: "pl",
     name: "Poland",
-    code: "PL",
   },
   {
-    name: "Czech",
-    code: "CZ",
+    code: "cz",
+    name: "Czech Republic",
+  },
+  {
+    code: "hu",
+    name: "Hungary",
+  },
+  {
+    code: "sk",
+    name: "Slovak Republic",
+  },
+  {
+    code: "bg",
+    name: "Bulgaria",
+  },
+  {
+    code: "de",
+    name: "Germany",
+  },
+  {
+    code: "es",
+    name: "Spain",
+  },
+  {
+    code: "ro",
+    name: "Romania",
+  },
+  {
+    code: "ru",
+    name: "Russia",
+  },
+  {
+    code: "rs",
+    name: "Serbia",
+  },
+  {
+    code: "cn",
+    name: "China",
+  },
+  {
+    code: "fr",
+    name: "France",
+  },
+  {
+    code: "hr",
+    name: "Croatia",
+  },
+  {
+    code: "at",
+    name: "Austria",
+  },
+  {
+    code: "pt",
+    name: "Portugal",
+  },
+  {
+    code: "si",
+    name: "Slovenia",
   },
 ];
 
 const users = [
   {
-    email: "tazo@gmail.com",
+    email: "tazo90@gmail.com",
     password: "pass",
     username: "tazo90",
     name: "ala",
     plan: UserPlan.FREE,
   },
   {
-    email: "team2pro@example.com",
-    password: "teampro",
+    email: "dieselo@o2.pl",
+    password: "diesel",
     username: "teampro",
     name: "Team Pro Example",
     plan: UserPlan.PRO,
