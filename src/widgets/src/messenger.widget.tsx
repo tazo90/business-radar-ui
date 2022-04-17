0cf1aa8c4df5b64c00ff7a47b9ff046a14913874
tazo90 <tazo90@gmail.com>
2022-04-18 00:36:19 +0200

    Remove messenger widget

--- src/widgets/src/messenger.widget.tsx
+++
@@ -1,9 +0,0 @@
-import { withWidget } from "./hooks/with-widget";
-import Messenger from "@pages/debug/messenger";
-
-function MessengerWidget() {
-  console.log("Messenger");
-  return <Messenger />;
-}
-
-export default withWidget("messenger", MessengerWidget);
