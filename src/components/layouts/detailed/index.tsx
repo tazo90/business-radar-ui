3dfd7af95733ae6ef7f49045a86a6f9263816ead
tazo90 <tazo90@gmail.com>
2022-04-27 00:42:13 +0200

    Rewrite DetailedLayout component

--- src/components/layouts/detailed/index.tsx
+++ src/components/layouts/detailed/index.tsx
@@ -1,34 +1,39 @@
 import PageHeader from "@components/common/page-header";
 import PageMenu from "@components/common/page-menu";
 
-export default function DetailedLayout({
-  children,
-  header,
-  pageTitle,
-  pageMenu,
-  pageDescription = "",
-}) {
+type DetailedLayoutProps = {
+  children: React.ReactNode;
+  pageTitle: string;
+  pageMenu: any;
+  pageAction?: React.ReactNode;
+  pageDescription?: string;
+};
+
+export default function DetailedLayout(props: DetailedLayoutProps) {
   return (
     <div className="flex flex-col">
-      <PageHeader>{header}</PageHeader>
+      <PageHeader />
       <div className="lg:grid lg:grid-cols-12">
-        <PageMenu menu={pageMenu} />
+        <PageMenu menu={props.pageMenu} />
 
         <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
           <div className="shadow sm:rounded-md sm:overflow-hidden">
             <div className="bg-white py-4 px-6 space-y-4 sm:py-4 sm:px-6">
-              <div>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  {pageTitle}
-                </h3>
-                {pageDescription && (
-                  <p className="mt-1 text-sm text-gray-500">
-                    {pageDescription}
-                  </p>
-                )}
+              <div className="flex justify-between">
+                <div className="flex flex-col">
+                  <h3 className="text-lg leading-6 font-medium text-gray-900">
+                    {props.pageTitle}
+                  </h3>
+                  {props.pageDescription && (
+                    <p className="mt-1 text-sm text-gray-500">
+                      {props.pageDescription}
+                    </p>
+                  )}
+                </div>
+                {props.pageAction}
               </div>
             </div>
-            {children}
+            {props.children}
           </div>
         </div>
       </div>
