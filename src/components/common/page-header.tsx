2bdfa221625ee5a6c51fdbbcd28d2115164cd726
tazo90 <tazo90@gmail.com>
2022-04-27 00:41:00 +0200

    Small UI refactoring

--- src/components/common/page-header.tsx
+++ src/components/common/page-header.tsx
@@ -1,12 +1,18 @@
 import Breadcrumb from "./breadcrumb";
 
-export default function PageHeader({ children }) {
+type PageHeaderProps = {
+  children?: React.ReactNode;
+};
+
+export default function PageHeader(props: PageHeaderProps) {
   return (
-    <div className="bg-white border-b border-gray-200 px-4 py-1.5 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+    <div className="bg-white border-b border-gray-200 px-4 py-2.5 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
       <div className="flex-1 min-w-0">
         <Breadcrumb />
       </div>
-      <div className="mt-4 flex sm:mt-0 sm:ml-0">{children}</div>
+      {props.children && (
+        <div className="mt-4 flex sm:mt-0 sm:ml-0">{props.children}</div>
+      )}
     </div>
   );
 }
--- src/pages/projects/[slug]/index.tsx
+++ src/pages/projects/[slug]/index.tsx
@@ -55,14 +55,6 @@ export default function ProjectPage() {
       pageMenu={projectMenu}
       pageTitle="Overview"
       pageDescription="You can customize your plan."
-      header={
-        <button
-          type="button"
-          className="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3"
-        >
-          Create application
-        </button>
-      }
     >
       <form action="#" method="POST">
         <div className="shadow sm:rounded-md sm:overflow-hidden">
