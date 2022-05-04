11d244fbfab590e006647dbbda3c0150e7c52e3e
tazo90 <tazo90@gmail.com>
2022-05-04 21:44:16 +0200

    Add profile settings page

--- src/pages/settings/profile.tsx
+++ src/pages/settings/profile.tsx
@@ -1,5 +1,7 @@
 import DashboardLayout from "@components/layouts/dashboard";
 import DetailedLayout from "@components/layouts/detailed";
+import Card from "@components/ui/card";
+import { Form, TextField } from "@components/ui/form/fields";
 import {
   CreditCardIcon,
   KeyIcon,
@@ -7,6 +9,7 @@ import {
 } from "@heroicons/react/solid";
 
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 
 export const appMenu = [
   {
@@ -26,10 +29,60 @@ export const appMenu = [
   },
 ];
 
+function ProfileForm() {
+  const form = useForm();
+
+  return (
+    <Form
+      form={form}
+      handleSubmit={(values) => console.log("SUBMIT", values)}
+      className="space-y-6"
+    >
+      <Card>
+        <Card.Header title="Profile" subtitle="Edit your profile information" />
+        <Card.Content>
+          <TextField
+            label="Username"
+            {...form.register("username", { required: true })}
+            type="text"
+            wrapperClassName="col-span-4 sm:col-span-2"
+          />
+          <TextField
+            label="Name"
+            {...form.register("name", { required: true })}
+            type="text"
+            wrapperClassName="col-span-4 sm:col-span-2"
+          />
+          <TextField
+            label="Email"
+            {...form.register("email", { required: true })}
+            type="text"
+            wrapperClassName="col-span-4 sm:col-span-2"
+          />
+        </Card.Content>
+        <Card.Footer>
+          <button
+            type="submit"
+            className="bg-gray-800 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+          >
+            Save
+          </button>
+        </Card.Footer>
+      </Card>
+    </Form>
+  );
+}
+
 export default function ProfilePage() {
   const { query } = useRouter();
 
-  return <DetailedLayout pageMenu={appMenu}>Profile</DetailedLayout>;
+  return (
+    <DetailedLayout pageMenu={appMenu}>
+      <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
+        <ProfileForm />
+      </div>
+    </DetailedLayout>
+  );
 }
 
 ProfilePage.Layout = DashboardLayout;
